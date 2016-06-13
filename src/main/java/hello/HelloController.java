de393510397438a9c7d31736fc0be42af30082d8
Rijul1204 <rashedulhasanrijul@gmail.com>
2016-06-14 00:30:07 +0800

    added websocket

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 @Controller
 public class HelloController {
@@ -15,9 +17,21 @@ public class HelloController {
         return "Greetings from Spring Boot!";
     }
     
+    @RequestMapping("/websock")
+    public String websock() {
+        return "index";
+    }
+    
     @RequestMapping("/calculator")
     public String getCalculator(Model model) {
         return "calculator";
+    }
+    
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(3000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
     }
 
 }
