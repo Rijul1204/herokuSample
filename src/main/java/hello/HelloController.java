d25a73e9f2dcfc5bf6e8f0cab375c06ff34a6002
Rashedul Hasan Rijul <rashedulhasanrijul@gmail.com>
2017-08-26 16:05:47 +0800

    removed unnecessary files

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,42 +1,22 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import hello.service.JavaCVService;
-import hello.service.PdfBoxService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 
 @Controller
 public class HelloController {
 	
-	@Autowired
-	JavaCVService javaCVService;
-	
-	@Autowired
-	PdfBoxService pdfService;
-
     @RequestMapping("/")
     @ResponseBody
     public String index() {
         return "Greetings from Spring Boot!";
-    }
-    
-    @RequestMapping("/java-cv")
-    @ResponseBody
-    public String runJavaCV() {
-    	javaCVService.smooth("C:\\Users\\DOLPHIN-PC\\Desktop\\pics\\building.jpg");
-        return "Running Java CV";
-    }
+    }    
     
     @RequestMapping("/websock")
     public String websock() {
@@ -51,14 +31,8 @@ public class HelloController {
     @RequestMapping("/calculator")
     public String getCalculator(Model model) {
         return "calculator";
-    }
-    
-    @RequestMapping("/readPdf")
-    @ResponseBody
-    public String readPdf(Model model) {
-    	return pdfService.readPdf();
-    }
-    
+    }    
+        
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
